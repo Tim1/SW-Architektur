0bbc6a94feb90af46931ec985985d80cd2568d24
DoubleSteakhouse <double.steakhouse@gmail.com>
2013-04-16 13:43:20 +0200

    #26 update: kleine beschreibung in AbstractBe.java hinzugefügt.

--- src/swa/runningeasy/data/be/AbstractBE.java
+++ src/swa/runningeasy/data/be/AbstractBE.java
@@ -1,5 +1,16 @@
 package swa.runningeasy.data.be;
 
+/**
+ * @author Whopper
+ * 
+ *         BE stands for Business Entity.
+ *         <p>
+ *         They are only for creating and updating Information to data base/
+ *         persistent layer.
+ *         <p>
+ *         DTO (Data Transfer Object) are only for Client-Server Communication,
+ *         not for the persistence layer.
+ * */
 public abstract class AbstractBE implements ISerializable {
 
 }
