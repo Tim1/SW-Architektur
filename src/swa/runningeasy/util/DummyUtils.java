5415e35e47308d2fb3336e49f2d30ea4fe7f2eac
DoubleSteakhouse <double.steakhouse@gmail.com>
2013-05-27 14:14:35 +0200

    #39 - Log4J 1.2.14 eingebunden und in DummyUtils getestet. - Javadocs konnte ich nicht einbinden. #41 - Konfiguration der Logger ist bis jetzt in der log4j-sample.properties beschrieben 	- MyConsoleAppender 	- MyFileAppender, welches eine .html-Datei in /logs schreibt. - Zu klären: 	- Konfiguration per XML, programmatisch oder mit .properties-file? 	- Wie soll die html-Datei aussehen?

--- src/swa/runningeasy/util/DummyUtils.java
+++ src/swa/runningeasy/util/DummyUtils.java
@@ -1,9 +1,23 @@
 package swa.runningeasy.util;
 
+
+import org.apache.log4j.Logger;
+import org.apache.log4j.PropertyConfigurator;
+
 /**
  * @author Tim Schmiedl (Cyboot)
  * 
  */
 public class DummyUtils {
+	private static Logger	logger	= Logger.getLogger(DummyUtils.class);
 
+	DummyUtils() {
+		logger.info("Meine Info-Meldung aus DummyUtils.");
+		logger.error("Meine Error-Meldung aus DummyUtils.");
+	}
+
+	public static void main(String[] args) {
+		PropertyConfigurator.configureAndWatch("log4j-sample.properties", 10 * 1000);
+		new DummyUtils();
+	}
 }
