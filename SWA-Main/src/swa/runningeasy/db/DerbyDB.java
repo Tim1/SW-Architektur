af352495b22462a9138ed7edcc3b0415e77e5602
Cyboot <Cyboot@timweb.de>
2013-06-11 21:48:49 +0200

    fixed gitignore & added missing file

---
+++ SWA-Main/src/swa/runningeasy/db/DerbyDB.java
@@ -0,0 +1,79 @@
+/**
+ * 
+ */
+package swa.runningeasy.db;
+
+import java.util.List;
+
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
+import javax.persistence.Query;
+
+import org.apache.log4j.Logger;
+
+/**
+ * @author Tim Schmiedl (Cyboot)
+ * 
+ */
+public class DerbyDB implements IObjectReader, IObjectWriter {
+	private static final String		PERSISTENCE_UNIT_NAME	= "runningeasy";
+	private static Logger			logger					= Logger.getLogger(DerbyDB.class);
+
+	private static EntityManager	em;
+
+	public static void init() {
+		EntityManagerFactory factory = Persistence.createEntityManagerFactory(PERSISTENCE_UNIT_NAME);
+		em = factory.createEntityManager();
+	}
+
+	@Override
+	public <C> C getObjectById(final Class<C> clazz, final Long id) {
+		logger.trace("call getObjectById()-method");
+		return null;
+	}
+
+	@Override
+	public <C> List<C> getObjectByValues(final Class<C> clazz, final Object dto) {
+		logger.trace("call getObjectByValues()-method");
+		return null;
+	}
+
+	@Override
+	public <C> List<C> getAllObjects(final Class<C> clazz) {
+		logger.trace("call getAllObjects()-method");
+		Query query = em.createQuery("select x from " + clazz.getSimpleName() + " x");
+
+		@SuppressWarnings("unchecked")
+		List<C> resultList = query.getResultList();
+		return resultList;
+	}
+
+	@Override
+	public <C> void save(final Class<C> clazz, final C objectToSave) {
+		logger.trace("call save()-method");
+		em.persist(objectToSave);
+	}
+
+	@Override
+	public <C> void delete(final Class<C> clazz, final C objectToDelete) {
+		logger.trace("call delete()-method");
+		em.remove(objectToDelete);
+	}
+
+	@Override
+	public void commit() {
+		logger.debug("commiting DB-Transaction");
+		em.getTransaction().commit();
+	}
+
+	@Override
+	public void begin() {
+		logger.debug("beginning DB-Transaction");
+		em.getTransaction().begin();
+	}
+
+	public static void close() {
+		em.close();
+	}
+}
