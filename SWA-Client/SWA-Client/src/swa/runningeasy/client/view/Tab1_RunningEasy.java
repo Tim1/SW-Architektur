71291abe0d47575021111f603d4d78b072e0fd51
DoubleSteakhouse <double.steakhouse@gmail.com>
2013-06-26 16:48:42 +0200

    #57 updateted Ta11_RunningEasy
    bei Vollbild siehts doof aus, aber bei den 3 Elementen laesst es sich eh
    nicht schoner machen..

--- SWA-Client/SWA-Client/src/swa/runningeasy/client/view/Tab1_RunningEasy.java
+++ SWA-Client/SWA-Client/src/swa/runningeasy/client/view/Tab1_RunningEasy.java
@@ -3,16 +3,18 @@
  */
 package swa.runningeasy.client.view;
 
-import java.awt.BorderLayout;
-import java.awt.Button;
-import java.awt.GridLayout;
-import java.awt.Label;
+import java.awt.Dimension;
+import java.awt.GridBagConstraints;
+import java.awt.GridBagLayout;
 import java.awt.image.BufferedImage;
 import java.io.File;
 import java.io.IOException;
 
 import javax.imageio.ImageIO;
+import javax.swing.Box;
+import javax.swing.BoxLayout;
 import javax.swing.ImageIcon;
+import javax.swing.JButton;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
 import javax.swing.JTextArea;
@@ -23,43 +25,65 @@ import javax.swing.JTextArea;
  */
 public class Tab1_RunningEasy extends JPanel {
 	private static final long	serialVersionUID	= 1L;
+	private JButton				butConnect;
+	private JButton				butDeleteDB;
+	private JLabel				picLabel;
+	private JTextArea			txtArea;
 
 	public Tab1_RunningEasy() {
 		initJPanel();
 	}
 
 	private void initJPanel() {
-		// layougt of Tab1_RunningEasy JPanel
-		// this.setLayout(new GridLayout(0, 2, 5, 10));
-		this.setLayout(new BorderLayout(10, 10));
 
-		// create Property panel
-		JPanel panelProperties = new JPanel(new GridLayout(0, 1));
-		Button buttonConnectDB = new Button("Verbinde mit DB");
-		Button buttonClearDB = new Button("Alle Daten Löschen");
-		panelProperties.add(buttonConnectDB);
-		panelProperties.add(buttonClearDB);
-		this.add(panelProperties, BorderLayout.WEST);
+		// initialize components
+		butConnect = new JButton("Mit DB verbinden");
+		butConnect.setAlignmentX(CENTER_ALIGNMENT);
+		butConnect.setMaximumSize(new Dimension(300, 25));
+		butDeleteDB = new JButton("Alle Daten löschen");
+		butDeleteDB.setAlignmentX(CENTER_ALIGNMENT);
+		butDeleteDB.setMaximumSize(new Dimension(300, 25));
+		txtArea = new JTextArea("RunningeEasy\nSoftware-Architektur\nVersion 1.0\nTim Schmiedl, Milos Babic");
 
-		// create picture label
 		BufferedImage myPicture;
 		try {
 			myPicture = ImageIO.read(new File("img\\bg.png"));
-			JLabel picLabel = new JLabel(new ImageIcon(myPicture));
-			this.add(picLabel, BorderLayout.EAST);
+			picLabel = new JLabel(new ImageIcon(myPicture));
 		} catch (IOException e) {
-			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
 
-		// create Descritpino-JPanel
-		JPanel panelInfo = new JPanel((new GridLayout(0, 1)));
-		panelInfo.add(new Label("App-Info"));
-		panelInfo.add(new JTextArea("Lorem ipsum dolor sit amet...."));
-		this.add(panelInfo, BorderLayout.SOUTH);
+		// create panel for buttons
+		JPanel buttonPanel = new JPanel();
+		BoxLayout boxLayoutbuttonPanel = new BoxLayout(buttonPanel, BoxLayout.Y_AXIS);
+		buttonPanel.setLayout(boxLayoutbuttonPanel);
+		buttonPanel.add(Box.createVerticalGlue());
+		buttonPanel.add(Box.createVerticalGlue());
+		buttonPanel.add(butConnect);
+		buttonPanel.add(butDeleteDB);
+
+		// set layout for Tab1
+		this.setLayout(new GridBagLayout());
+		GridBagConstraints c = new GridBagConstraints();
+
+		// add components
+		c.fill = GridBagConstraints.HORIZONTAL;
+		c.gridx = 0;
+		c.gridy = 0;
+		this.add(buttonPanel, c);
+
+		c.fill = GridBagConstraints.HORIZONTAL;
+		c.gridx = 1;
+		c.gridy = 0;
+		this.add(picLabel, c);
+
+		c.fill = GridBagConstraints.HORIZONTAL;
+		c.gridx = 0;
+		c.gridwidth = 2;
+		c.gridy = 1;
+		this.add(txtArea, c);
+
 
 		this.setVisible(true);
 	}
-
-
 }
